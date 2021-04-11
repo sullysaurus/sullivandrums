e03b3960ac09933ffe1717a8c5780b9b5573ebca
sullysaurus <dsully15@gmail.com>
2021-04-11 10:09:02 -0400

    readme updates and title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Dimension V2',
-    author: 'Hunter Chang',
-    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
+    title: 'Sullivan Drums | Raleigh, NC',
+    author: 'Danny Sullivan',
+    description: 'Drummer based in Raleigh, NC',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
